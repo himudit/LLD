daac8beeb7ad82f789ca8b36dc490af4c78727cc
himudit <gargmudit662@gmail.com>
2025-10-24 22:12:26 +0530

    Open/closed Principle

--- LLD.cpp
+++ LLD.cpp
@@ -2,3 +2,31 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+// Open-Closed Principle
+class Shape
+{
+public:
+    virtual double area() = 0;
+};
+
+class Rectangle : public Shape
+{
+    double w, h;
+
+public:
+    Rectangle(double width, double height)
+    {
+        w = width;
+        h = height;
+    }
+    double area()
+    {
+        return w * h;
+    }
+};
+
+int main(){
+    Rectangle rect(4,5);
+    cout << rect.area();
+}
