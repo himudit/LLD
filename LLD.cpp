743f33ac3aa9d159a3cf1d53460a56b0f6596528
himudit <gargmudit662@gmail.com>
2025-10-26 19:27:31 +0530

    abstract factory pattern

--- LLD.cpp
+++ LLD.cpp
@@ -26,35 +26,78 @@ public:
     }
 };
 
-int main(){
-    Rectangle rect(4,5);
+int main()
+{
+    Rectangle rect(4, 5);
     cout << rect.area();
 }
 
 // Factory Design Pattern
 // what is static
 // what is ::
-class Vehicle{
-    public:
+class Vehicle
+{
+public:
     virtual void printType() = 0;
     virtual ~Vehicle() {}
 };
-class Car: public Vehicle{
-    public:
-    void printType() override{
-      cout << "This is a Car" << endl;   
+class Car : public Vehicle
+{
+public:
+    void printType() override
+    {
+        cout << "This is a Car" << endl;
     }
 };
-class vehicleFactory{
+class vehicleFactory
+{
 public:
-    static Vehicle* getVehicle(string v){
-        if(v == "car"){
+    static Vehicle *getVehicle(string v)
+    {
+        if (v == "car")
+        {
             return new Car();
         }
         return nullptr;
     }
 };
-int main(){
-    Vehicle* v1 = vehicleFactory::getVehicle("car");
+int main()
+{
+    Vehicle *v1 = vehicleFactory::getVehicle("car");
     v1->printType();
+}
+
+// Abstract Factory Pattern
+// Manage Different Car Brands
+class Vehicle
+{
+public:
+    virtual void printType() = 0;
+    virtual ~Vehicle() {}
+};
+class Honda : public Vehicle
+{
+public:
+    void printType() override
+    {
+        cout << "I am of Honda Type";
+    }
+};
+class vehicleFactory
+{
+public:
+    virtual Vehicle* createVehicle() = 0;
+};
+class HondaFactory : public vehicleFactory
+{
+public:
+    Vehicle* createVehicle() override
+    {
+        return new Honda();
+    }
+};
+
+int main(){
+    vehicleFactory* factory = new HondaFactory();
+    Vehicle* honda = factory->createVehicle();
 }
