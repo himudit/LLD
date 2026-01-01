5a154124b7ae0c9b7cd6d0900828cabd54b3856c
himudit <gargmudit662@gmail.com>
2026-01-01 22:41:47 +0530

    Prototype Pattern

--- Creational Design Pattern/Prototype_Pattern.cpp
+++ Creational Design Pattern/Prototype_Pattern.cpp
@@ -3,6 +3,53 @@
 #include <string>
 using namespace std;
 
+class CharacterPrototype
+{
+public:
+    virtual void show() = 0;
+    virtual CharacterPrototype *clone() = 0;
+    virtual ~CharacterPrototype() {}
+};
+
+class GameCharacter : public CharacterPrototype
+{
+private:
+    string name;
+    int health;
+    string weapon;
+
+public:
+    void setName(string n)
+    {
+        this->name = n;
+    }
+    void setHealth(int h)
+    {
+        this->health = h;
+    }
+    void setWeapon(string w)
+    {
+        this->weapon = w;
+    }
+    void show()
+    {
+        cout << this->name << " " << this->health << " " << this->weapon << endl;
+    }
+    GameCharacter *clone()
+    {
+        return new GameCharacter(*this);
+    }
+};
+
 int main(){
-    
+    GameCharacter *prototype = new GameCharacter();
+    prototype->setName("Default Name");
+    prototype->setHealth(100);
+    prototype->setWeapon("Default Weapon");
+
+    GameCharacter *guts = prototype->clone();
+    guts->setName("Guts");
+    guts->setWeapon("Blade");
+
+    guts->show();
 }
