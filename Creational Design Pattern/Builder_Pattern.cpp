dc21dd23a0a24675e78559eb7e832156993b0fd0
himudit <gargmudit662@gmail.com>
2026-04-07 19:54:42 +0530

    Refactor HTTPBuilder to eliminate direct object mutation and streamline method handling

--- Creational Design Pattern/Builder_Pattern.cpp
+++ Creational Design Pattern/Builder_Pattern.cpp
@@ -1,6 +1,4 @@
 #include <bits/stdc++.h>
-#include <iostream>
-#include <string>
 using namespace std;
 
 enum Method
@@ -20,9 +18,6 @@ private:
     string body;
 
 public:
-    HTTP()
-    {
-    }
     HTTP(const Method &m, const string &headers, const string &query_params, const string &body) : method(m), headers(headers), query_params(query_params), body(body) {}
 
     void showDetails()
@@ -37,52 +32,48 @@ public:
 class HTTPBuilder
 {
 private:
-    HTTP http;
+    Method method;
+    bool hasMethod = false;
+
+    string headers;
+    string query_params;
+    string body;
 
 public:
-    HTTPBuilder &setMethod(Method m)
+    HTTPBuilder(Method m)
     {
-        http.method = m;
-        return *this;
+        method = m;
     }
     HTTPBuilder &setHeaders(string h)
     {
-        http.headers = h;
+        headers = h;
         return *this;
     }
     HTTPBuilder &setQueryParams(string qp)
     {
-        http.query_params = qp;
+        query_params = qp;
         return *this;
     }
     HTTPBuilder &setBody(string b)
     {
-        http.body = b;
+        body = b;
         return *this;
     }
     HTTP build()
     {
-        return http;
+        return HTTP(method, headers, query_params, body);
     }
 };
 int main()
 {
-    HTTP http1 = HTTPBuilder()
+    HTTP http1 = HTTPBuilder(Method::POST)
                      .setBody("id:3930,name:mudit")
                      .setHeaders("application/json")
                      .setQueryParams("page=4")
-                     .setMethod(Method::POST)
                      .build();
     http1.showDetails();
-
-    HTTP http2 = HTTPBuilder()
-                     .setBody("id:600,name:garg")
-                     .setHeaders("json")
-                     .setQueryParams("page=1")
-                     .setMethod(Method::GET)
-                     .build();
-    http2.showDetails();
 }
 
 // friend class HTTPBuilder -> helps HTTPBuilder to access Private/Protected Data Members.
 // Never make Builder in Abstract class it must be in Concrete class.
+// Builder should not mutate the Object directly.
